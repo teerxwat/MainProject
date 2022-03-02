68a51e6d4a7b5ff0a856179df852d4dc613ea137
mOrales <teerawatpinchai14@gmail.com>
2022-03-02 20:35:58 +0700

    LAB1

---
+++ ProjectHelloWorldLAB01/src/Project/HelloWorld.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
